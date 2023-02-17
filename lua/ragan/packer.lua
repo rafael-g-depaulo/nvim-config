668329290ff77e75be5ea62bbff79810d32b8741
Rafael G. de Paulo <rafael.g.depaulo@gmail.com>
2023-02-17 20:27:24 -0300

    add primeagen recommended plugins

--- lua/ragan/packer.lua
+++ lua/ragan/packer.lua
@@ -10,8 +10,17 @@ return require('packer').startup(function(use)
     use {
 	    'nvim-telescope/telescope.nvim', tag = '0.1.1',
 	    -- or                            , branch = '0.1.x',
+
+
 	    requires = { {'nvim-lua/plenary.nvim'} }
     }
+
+    use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
+    use('nvim-treesitter/playground')
+
+    -- file navigation. made by the youtube vim guy
+    use('theprimeagen/harpoon')
+    use('mbbill/undotree')
 
     -- themes
     use 'tomasiser/vim-code-dark'
